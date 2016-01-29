24f5ab45b2c7bd72274a96d26bfbb89c3b5aab1a
Douglas Hutchings <11douglash@berkeley.edu>
2016-01-29 11:26:18 -0800

    There are now compile & upload scripts.
    Also added compiled_on.txt and git_log.txt to the .gitignore, since these are generated.

---
+++ load_ros.sh
@@ -0,0 +1,36 @@
+ #!/bin/bash         
+
+#need to install 'sshpass'
+echo "Loading both Mbed and Zumy Files"
+echo "You'd better be ready for the possibility of loosing files"
+echo ""
+
+NAME="$1"
+echo "Loading to " $NAME
+
+echo "Deleting everything"
+ssh "zumy@"$NAME".local" "rm -rf *"
+
+echo "Loading ROS code"
+cd on_zumy_odroid/
+scp -r * "zumy@"$NAME".local:"
+cd ../
+
+#echo "Loading Mbed .bin file"
+#cd zumy_mbed/ 
+#copy the bin file over to a temp location
+#scp "zumy_mbed.bin" "zumy@"$NAME".local:"
+
+#ssh in and mount the mbed files.  I'm going to need root for this
+#spawn sshpass -p "zumy" ssh -o StrictHostKeyChecking=no "zumy@"$NAME".local"
+
+#ssh -t "zumy@"$NAME".local" "sudo mkdir -p "/media/boot/mbed" ; sudo mount "/dev/sda" "/media/boot/mbed/" ; sudo cp -f "/home/zumy/zumy_mbed.bin" "/media/boot/mbed" ; sync"
+
+#make a file with the date & time, but it into the zumy for informational purposes
+dt=`date '+%d/%m/%Y %H:%M:%S'`
+echo "$dt" > "last_loaded.txt"
+scp "last_loaded.txt" "zumy@"$NAME".local:"
+
+exit
+
+
