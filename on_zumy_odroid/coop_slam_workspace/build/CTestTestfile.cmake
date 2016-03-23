fdfa4397f1b922baaafa7fb3adcb5c6165dd9313
Douglas Hutchings <11douglash@berkeley.edu>
2016-03-23 15:10:05 -0700

    Many changes to .gitignore.
    Now i ignore all of the build files i accidentally comitted before

--- on_zumy_odroid/coop_slam_workspace/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/doug/Desktop/ee106_zumy_support/on_zumy_odroid/coop_slam_workspace/src
-# Build directory: /home/doug/Desktop/ee106_zumy_support/on_zumy_odroid/coop_slam_workspace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(odroid_machine)
--- on_zumy_odroid/coop_slam_workspace/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/doug/Desktop/ee106_zumy_support/on_zumy_odroid/coop_slam_workspace/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
