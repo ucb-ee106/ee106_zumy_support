15a9cfbb706451b99c4c3bcf2f0ab9626f041855
Douglas Hutchings <11douglash@berkeley.edu>
2016-03-24 14:30:23 -0700

    Removed /build file from git commits.

--- on_zumy_odroid/coop_slam_workspace/build/odroid_machine/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/doug/Desktop/ee106_zumy_support/on_zumy_odroid/coop_slam_workspace/src/odroid_machine
-# Build directory: /home/doug/Desktop/ee106_zumy_support/on_zumy_odroid/coop_slam_workspace/build/odroid_machine
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
