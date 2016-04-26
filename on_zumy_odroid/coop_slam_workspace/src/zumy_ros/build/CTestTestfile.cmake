237e01918b813fe964465c8ae4b21f6cf2918ead
Douglas Hutchings <11douglash@berkeley.edu>
2016-04-26 12:32:55 -0700

    Removed build and devel files from git history on zumy_ros

--- on_zumy_odroid/coop_slam_workspace/src/zumy_ros/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/doug/Desktop/ee106_zumy_support/on_zumy_odroid/zumy_ros/src
-# Build directory: /home/doug/Desktop/ee106_zumy_support/on_zumy_odroid/zumy_ros/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(ros_zumy)
